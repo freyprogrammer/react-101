0f255961354d7ecc7021079a0fc34d9cfdfdb69c
frey <freyprogrammer@gmail.com>
2019-12-19 23:06:15 +0800

    refresher on state and components

---
+++ react-mosh/src/components/counter.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+
+export class Counter extends Component {
+    state = {
+        count: 0,
+    };
+
+    render() {
+        return (
+            <React.Fragment>
+                <span>{this.formatCount()}</span>
+                <button className='btn btn-primary'>Increment</button>
+            </React.Fragment>
+        )
+    }
+
+    formatCount(){
+        const { count } = this.state;
+        return count === 0 ? <h3>Zero</h3> : count;  
+    }
+}
+
+export default Counter;
