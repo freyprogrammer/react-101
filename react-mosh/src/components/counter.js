dd9d5af3580f1af736c885381257fb0123c0f69b
frey <freyprogrammer@gmail.com>
2019-12-30 19:19:58 +0800

    create controlled component

--- react-mosh/src/components/counter.js
+++ react-mosh/src/components/counter.js
@@ -1,9 +1,7 @@
 import React, { Component } from 'react'
 
 export class Counter extends Component {
-    state = {
-        count: this.props.counter.value
-    };
+
     styles = {
         fontSize: 50,
         fontWeight: "bold"
@@ -14,18 +12,14 @@ export class Counter extends Component {
     //     this.handleIncrement = this.handleIncrement.bind(this);
     // }
 
-    handleIncrement = product => {
-        this.setState({
-            count: this.state.count + 1
-        })
-    }
-
     render() {
         return (
             <div>
                 <h2>Counter #{this.props.counter.id}</h2>
-                <span style={this.styles} className={this.getBadgeClasses()}>{this.formatCount()}</span>
-                <button onClick={() => this.handleIncrement("product")} className='btn btn-secondary m-2'>Increment</button>
+                <span style={this.styles} className={this.getBadgeClasses()}>
+                    {this.formatCount()}
+                </span>
+                <button  onClick={() => this.props.onIncrement(this.props.counter)} className='btn btn-secondary m-2'>Increment</button>
 
                 <button onClick={() => this.props.onDelete(this.props.counter.id)} className='btn btn-danger m-2'>Delete</button>
             </div>
@@ -34,13 +28,13 @@ export class Counter extends Component {
 
     getBadgeClasses() {
         let classCount = "badge m-2 badge-";
-        classCount += this.state.count === 0 ? "warning" : "primary";
+        classCount += this.props.counter.value === 0 ? "warning" : "primary";
         return classCount;
     }
 
     formatCount(){
-        const { count } = this.state;
-        return count === 0 ? "0": count;  
+        const { value } = this.props.counter;
+        return value === 0 ? "0": value;
     }
 }
 
--- react-mosh/src/components/counters.js
+++ react-mosh/src/components/counters.js
@@ -5,7 +5,7 @@ export class Counters extends Component {
     state = {
         counters: [
             {id: 1, value: 4},
-            {id: 2, value: 99},
+            {id: 2, value: 0},
             {id: 3, value: 0},
             {id: 4, value: 0},
             {id: 5, value: 0}
@@ -26,13 +26,26 @@ export class Counters extends Component {
         this.setState({ counters });
     }
 
+    handleIncrement = counter => {
+        const counters = [...this.state.counters];
+        const index = counters.indexOf(counter);
+        counters[index] = { ...counter };
+        counters[index].value++;
+        this.setState({ counters });
+
+    }
+
     render() {
         
         return (
             <div>
                 <button onClick={this.handleReset} className="btn btn-info btn-lg m-2">Reset</button>
                 {this.state.counters.map(counter => 
-                <Counter key={counter.id} onDelete={this.handleDelete} counter={counter}/>
+                <Counter 
+                key={counter.id} 
+                onDelete={this.handleDelete} 
+                onIncrement={this.handleIncrement} 
+                counter={counter}/>
                 )}
             </div>
         )
