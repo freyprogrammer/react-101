833305cf0781de99e6c3aa225e57cfed49105867
frey <freyprogrammer@gmail.com>
2020-01-11 10:27:32 +0800

    setup and install basic dependencies

---
+++ react-get/src/components/PostList.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import axios from 'axios'
+
+class PostList extends Component {
+    render() {
+        return (
+            <div>
+              List of Post  
+            </div>
+        )
+    }
+}
+
+export default PostList
